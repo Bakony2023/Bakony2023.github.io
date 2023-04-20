03fad90526a19c1d0b20b263375dc373d19176ed
Bakony2023 <131384431+Bakony2023@users.noreply.github.com>
2023-04-20 23:43:29 +0200

    added the login feature

---
+++ login.js
@@ -0,0 +1,14 @@
+const loginButton=document.querySelector('#loginButton');
+const select=document.querySelector('#selectUser');
+
+let currentUser=localStorage.getItem('user');
+if(currentUser!=null)
+{
+  window.location.href = 'calendar.html';
+}
+
+function login()
+{
+   localStorage.setItem('user',select.value);
+   window.location.href = 'calendar.html';
+}
